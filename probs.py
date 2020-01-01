433c6507860b7726be48d6017e57c0ab7a12b9ad
Michael Krasnitski <michael.krasnitski@gmail.com>
2020-01-01 01:59:59 -0500

    added basic prompt

--- probs.py
+++ probs.py
@@ -1,6 +1,35 @@
+import re
 import itertools
 import pandas as pd
 import matplotlib.pyplot as plt
+
+def t(raw=False):
+	if raw:
+		print(str(misc) + '\n')
+		print(sums)
+	else:
+		print(str(misc_percents.apply(fstr)) + '\n')
+		print(sum_percents.applymap(fstr))
+
+def r(low, high, raw=False):
+	if low is None:
+		low = 2
+	num = len(sum_data[(low <= sum_data['sum']) & (sum_data['sum'] < high)])
+	if raw:
+		print(num)
+	else:
+		print(fstr(100*num / len(data)))
+
+def g(raw=False):
+	ax = None
+	if raw:
+		ax = sums['='].plot.bar()
+		ax.set_ylabel('Raw Count')
+	else:
+		ax = sum_percents['='].plot.bar()
+		ax.set_ylabel('Percent Chance')
+	ax.set_xlabel('Sum')
+	plt.show()
 
 data = pd.DataFrame()
 data['outcome'] = list(itertools.product(range(1, 7), range(1, 7), (0, 0, 0, 1, 2, 3)))
@@ -27,34 +56,32 @@ for s in range(2, 16):
 sums['<='] = sums['<'] + sums['=']
 sums['>='] = sums['>'] + sums['=']
 
-
 fstr = '{:.1f}%'.format
 misc_percents = 100*misc/len(data)
 sum_percents = 100*sums/len(data)
 
-def t(raw=False):
-	if raw:
-		print(str(misc) + '\n')
-		print(sums)
-	print(str(misc_percents.apply(fstr)) + '\n')
-	print(sum_percents.applymap(fstr))
-
-def r(low, high=None, raw=False):
-	if not high:
-		high = low
-		low = 2
-	num = len(sum_data[(low <= sum_data['sum']) & (sum_data['sum'] < high)])
-	if raw:
-		print(num)
-	print(fstr(100*num / len(data)))
-
-def g(raw=False):
-	ax = None
-	if raw:
-		ax = sums['='].plot.bar()
-		ax.set_ylabel('Raw Count')
+# start event loop
+while True:
+	command = input('command: ').strip()
+	if command in ['q', 'quit', 'exit']:
+		break
+	elif command in ['t', 'table']:
+		t()
+	elif command in ['g', 'graph']:
+		g()
+	elif match := re.match(r'r(?:ange)?\((?:(\d+),)?\s*(\d+)\)', command):
+		low, high = match.groups()
+		if low:
+			low = int(low)
+		high = int(high)
+		r(low, high)
+	elif command in ['h', 'help']:
+		s = "list of commands\n" \
+		+ " - t[able]\t\t\tprint a table\n" \
+		+ " - g[raph]\t\t\tshow a graph\n" \
+		+ " - r[ange]([low,] high)\t\tpercent change of a sum in a range\n" \
+		+ " - q[uit], exit\t\t\tquit the program\n" \
+		+ " - h[elp]\t\t\tshow this help text\n"
+		print(s)
 	else:
-		ax = sum_percents['='].plot.bar()
-		ax.set_ylabel('Percent Chance')
-	ax.set_xlabel('Sum')
-	plt.show()
+		print('unknown command')
